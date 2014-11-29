b875a6cb883310e6f98bb7e5f1ba89c79b755d10
Cole Dunsby <coledunsby@gmail.com>
2014-11-29 12:52:19 -0500

    Worked on Signup/Login pages.

--- SEG3102-Project/src/java/beans/UserData.java
+++ SEG3102-Project/src/java/beans/UserData.java
@@ -5,10 +5,168 @@
  */
 package beans;
 
+import dbaccess.persistence.User;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
 /**
  *
  * @author Cole
  */
+@Named(value = "userData")
+@RequestScoped
 public class UserData {
+    private String id;
+    private String username;
+    private String password;
+    private String givenName;
+    private String lastName;
+    private String email;
+    private String type;
+    private String maxRent;
+    private String addstatus;
+    private List<User> lookupResults;
+ 
+    /**
+     * Creates a new instance of UserData
+     */
+    public UserData() {
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
+    /**
+     * @return the givenName
+     */
+    public String getGivenName() {
+        return givenName;
+    }
+
+    /**
+     * @param givenName the givenName to set
+     */
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+    
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the email to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    /**
+     * @return the maxRent
+     */
+    public String getmaxRent() {
+        return maxRent;
+    }
+
+    /**
+     * @param maxRent the maxRent to set
+     */
+    public void setMaxRent(String maxRent) {
+        this.maxRent = maxRent;
+    }
+    
+    public String getAddstatus() {
+        return addstatus;
+    }
+
+    public void setAddstatus(String addstatus) {
+        this.addstatus = addstatus;
+    }
+
+    public void setLookupResults(List<User> results) {
+        this.lookupResults = results;
+    }
+    
+    public List<User> getLookupResults() {
+        return lookupResults;
+    }
+    // show results if any
+    public boolean getShowResults() {
+        return (lookupResults != null) && !lookupResults.isEmpty();
+    }
+    // show message if no result
+    public boolean getShowMessage() {
+        return (lookupResults != null) && lookupResults.isEmpty();
+    }
 }
