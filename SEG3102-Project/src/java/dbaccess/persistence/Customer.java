f85682be76c826e339b6959ebf9ec0bddf71db96
Cole Dunsby <coledunsby@gmail.com>
2014-12-03 16:20:30 -0500

    Fixed conflicts.

--- SEG3102-Project/src/java/dbaccess/persistence/Customer.java
+++ SEG3102-Project/src/java/dbaccess/persistence/Customer.java
@@ -18,19 +18,12 @@ import javax.persistence.Table;
 @Entity
 @Table(name="Customer")
 public class Customer extends User implements Serializable {
-<<<<<<< HEAD
-    @OneToOne
-    protected Tenant tenant;
-    protected static final long serialVersionUID = 1L;
-    protected double maxRent;
-    @OneToOne
-=======
     @OneToOne(cascade = CascadeType.PERSIST)
     private Tenant tenant;
     private static final long serialVersionUID = 1L;
     private double maxRent;
     @OneToOne(cascade = CascadeType.PERSIST)
->>>>>>> 84cd1a1599e8dcb5a940eb2a049985404322fd78
+
     protected VisitingList visitingList = new VisitingList();
 
     public Customer() {
