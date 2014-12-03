11927d9a30ddf080026ece43a416a46ff09a59b6
Simon Northmore <mrpopcicals@gmail.com>
2014-12-03 16:25:00 -0500

    Stuff

--- SEG3102-Project/src/java/dbaccess/persistence/User.java
+++ SEG3102-Project/src/java/dbaccess/persistence/User.java
@@ -8,6 +8,8 @@ package dbaccess.persistence;
 import java.io.Serializable;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import static javax.persistence.InheritanceType.JOINED;
@@ -24,6 +26,7 @@ import javax.persistence.Table;
 @Inheritance(strategy=JOINED)
 public class User implements Serializable{
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     protected String id;
     @OneToOne(mappedBy = "user", cascade = CascadeType.PERSIST)
     protected UserAccount account;
